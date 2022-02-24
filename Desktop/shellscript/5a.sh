12242301609e699f5c4f5f2166747622fc265c4e
nishmirai1 <98949926+nishmirai1@users.noreply.github.com>
2022-02-24 14:26:02 +0530

    lab prg5a

--- Desktop/shellscript/5a.sh
+++ Desktop/shellscript/5a.sh
@@ -1,3 +1,5 @@
+#5a. Write a shell script that accepts filename as argument and display its creation time if file
+#exist and if does not send output error message.
 filename=$1
 if [ -e $filename ]
 then
