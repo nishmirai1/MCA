45b94321e7b85a21ee0a5c31ab1f3a040ded77dd
nishmirai1 <98949926+nishmirai1@users.noreply.github.com>
2022-02-24 14:35:11 +0530

    Add files via upload

--- Desktop/shellscript/6b.sh
+++ Desktop/shellscript/6b.sh
@@ -1,5 +1,3 @@
-#6b. Write a shell script to list all the files in a directory whose filename is at least 10 characters.
-#(use expr command to check the length).
 for i in `ls`
 do
 f1=`expr length $i`
