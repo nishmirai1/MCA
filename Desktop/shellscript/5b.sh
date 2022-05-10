335242a98c100a11dd479ecc1ba20edd0e5f5dcd
nishmirai1 <98949926+nishmirai1@users.noreply.github.com>
2022-05-10 16:20:17 +0530

    Update 5b.sh

--- Desktop/shellscript/5b.sh
+++ Desktop/shellscript/5b.sh
@@ -1,3 +1,5 @@
+#Write a shell script to display the calendar for current month with current date replaced by
+#*or**depending on whether the date has one digit or two digits
 date=`date +%d`
 date=`expr $date + 0`
 l=`cal | tr -d "[\137][\010]" | grep -wn "$date" | head -c 1`
