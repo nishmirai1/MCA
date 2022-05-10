d134103398684197b40563a52b32b61a66d4109d
nishmirai1 <98949926+nishmirai1@users.noreply.github.com>
2022-05-10 16:40:26 +0530

    Update 1a.sh

--- Desktop/shellscript/1a.sh
+++ Desktop/shellscript/1a.sh
@@ -1,3 +1,6 @@
+#1a.Write a shell script that takes a valid directory name as an argument and recursively descend all the sub-
+#directories,finds the maximum length of any file in that hierarchy and write s this maximum value to the standa
+#rd output.
 if [ $# -eq 1 ]
 then
 if [ -d $1 ]
